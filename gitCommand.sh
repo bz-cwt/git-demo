a89572029b40def2ba17b020205efaac18dcac0d
Beifei Zhou <beifei.zhou@carlsonwagonlit.com>
2019-01-17 14:49:24 +0000

    TaskId: Add commit

--- gitCommand.sh
+++ gitCommand.sh
@@ -1,5 +1,8 @@
 #Git commands
 git init
+git config --global 'Beifei Zhou'
+git config --global 'beifei.zhou@carlsonwagonlit.com'
 git status
 git add gitCommands.sh
 git commit -m 'TaskId: This is the git demo task'
+git remote add origin https://github.com/bz-cwt/git-demo.git
